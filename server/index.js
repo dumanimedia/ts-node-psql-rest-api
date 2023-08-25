a18074636936a6622ee619cc6896db1f9113b943
dumani <dumanimedia@gmail.com>
2023-08-25 13:05:08 +0300

    setup error middleware

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+dotenv.config();
+import { notFound, errorHandler } from './utils/middleware.js';
+const server = express();
+const PORT = process.env.PORT ?? 8080;
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(cookieParser());
+server.use(cors());
+server.use(notFound);
+server.use(errorHandler);
+server.listen(Number(PORT), () => console.log(`Server 🚀 on port ${PORT}!`));
+//# sourceMappingURL=index.js.map
---
+++ src/utils/middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import asyncHandler from 'express-async-handler';
+
+const notFound = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.status(404);
+    throw new Error(`Route not found - ${req.originalUrl}`);
+  }
+);
+
+async function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  const message = err.message;
+  const stack = process.env.NODE_ENV === 'development' ? err.stack : '';
+
+  res.status(statusCode).json({
+    message,
+    stack,
+  });
+}
+
+export { notFound, errorHandler };
