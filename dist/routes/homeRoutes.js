e3c8ca985f6618a282e5feb02667f0c8bc703211
dumani <dumanimedia@gmail.com>
2024-03-20 21:37:05 +0300

    Remove and make the dist file remote.

--- dist/routes/homeRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import homeController from "../controllers/homeController.js";
-const router = express.Router();
-router.route("/").get(homeController.getHomeResponse);
-export default router;
