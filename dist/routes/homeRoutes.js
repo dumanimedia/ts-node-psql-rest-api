82c01f3d301193b6c48f9e349346a94ea2c9f0d9
dumani <dumanimedia@gmail.com>
2024-03-20 21:43:13 +0300

    Remake the dist folder.

---
+++ dist/routes/homeRoutes.js
@@ -0,0 +1,5 @@
+import express from "express";
+import homeController from "../controllers/homeController.js";
+const router = express.Router();
+router.route("/").get(homeController.getHomeResponse);
+export default router;
